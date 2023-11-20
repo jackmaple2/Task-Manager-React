94713be5da4a2099149a4b3cd86adb194bfc4402
jackmaple2 <jackmaple3000@gmail.com>
2023-11-20 22:59:47 +0000

    adds addItemButtonPressed to onClick in EnterItem

--- taskmanager/src/components/EnterItem.jsx
+++ taskmanager/src/components/EnterItem.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function EnterItem(props) {
 
-    
+    const [newTask, setNewTask] =useState('');
+
+    function addItemButtonPressed() {
+        props.addItem({tasks: newTask});
+        setNewTask('');
+    }
 
     return (
         <div className="container">
             <div className="text-center">
                 <label htmlFor="task-field">New task: </label>
                 <input type="text"></input>
-                <button className="btn btn-primary" type="button" >Add Task</button>
+                <button className="btn btn-primary" type="button" onClick={addItemButtonPressed} >Add Task</button>
             </div>
         </div>
     )

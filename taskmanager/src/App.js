dd4b49337ba90caeb2e3a3f5d536afeac3ea1e32
jackmaple2 <jackmaple3000@gmail.com>
2023-11-20 22:54:12 +0000

    adds addTask function

--- taskmanager/src/App.js
+++ taskmanager/src/App.js
@@ -7,12 +7,31 @@ import Tasks from './components/Tasks';
 
 function App() {
 
-  const [tasks, setTasks] = useState('');
+  const [tasks, setTasks] = useState({tasks: []});
+
+  const addTask = (task) => {
+    let taskList = tasks['tasks'];
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(task)
+    };
+    fetch('http://localhost:3000/tasks', requestOptions)
+    .then((response) => {
+      response.json();
+    })
+    .then((data) => {
+      taskList.push(data);
+      setTasks(taskList);
+    })
+  }
 
   return (
     <div className="App">
       <Header />
-      <EnterItem tasks={tasks} setTasks={setTasks} />
+      <EnterItem addTask={addTask} tasks={tasks} setTasks={setTasks} />
       <Tasks tasks={tasks} setTasks={setTasks} />
     </div>
   );
--- taskmanager/src/components/EnterItem.jsx
+++ taskmanager/src/components/EnterItem.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 export default function EnterItem(props) {
 
+    
 
     return (
         <div className="container">
